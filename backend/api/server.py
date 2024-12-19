7d0feda3b9845e57f863654acc4c47dd7346144c
Armani Tuileepa <169069271+ArmaniTulip@users.noreply.github.com>
2024-12-19 15:09:32 -0700

    Add .env to .gitignore and include type stubs for various packages; update backend requirements

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,5 +1,6 @@
 # server.py
 from flask import Flask
+from flask_cors import CORS
 from api_routes import setup_routes
 import os
 from dotenv import load_dotenv
@@ -8,7 +9,8 @@ import openai
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 setup_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
